18924f9a2791e96824fb298e09fdd35e126a5da5
Xinyuan Chen <xinyuan.c@yahoo.com>
2025-05-30 17:55:23 -0700

    Initial Commit

---
+++ memory_llm/memory_llmApp.swift
@@ -0,0 +1,17 @@
+//
+//  memory_llmApp.swift
+//  memory_llm
+//
+//  Created by Xinyuan Chen on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct memory_llmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ memory_llmTests/memory_llmTests.swift
@@ -0,0 +1,17 @@
+//
+//  memory_llmTests.swift
+//  memory_llmTests
+//
+//  Created by Xinyuan Chen on 5/30/25.
+//
+
+import Testing
+@testable import memory_llm
+
+struct memory_llmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
